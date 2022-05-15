4fed387931653fb203803319eedfab1a037357d3
patrykdros97 <patryk.dros@gmail.com>
2022-05-15 22:36:57 +0200

    adding main.py file

---
+++ main.py
@@ -0,0 +1,9 @@
+from shop import app
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    
+    app.debug = True
+    app.run()
