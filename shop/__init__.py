b5af35beac0ed011a7c616e755e78b40defd79cf
patrykdros97 <patryk.dros@gmail.com>
2022-05-16 09:03:34 +0200

    changes on shop folder

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/mac-patryk/Dev/shop_backend/shop/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop/test.db'
 db = SQLAlchemy(app)
 
 from shop.products import models, routes
